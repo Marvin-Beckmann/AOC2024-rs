a9c888355b3a40118e6d06983d5a228ce7a7c6ab
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2024-12-08 13:53:48 +0100

    add solution for day 7

--- day07-2/src/main.rs
+++ day07-2/src/main.rs
@@ -1,3 +1,46 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    println!("{}", solve_2(puzzle_input));
+}
+
+pub fn solve_2(puzzle_input: String) -> usize {
+    puzzle_input
+        .lines()
+        .map(|x| {
+            let (target, remainder) = x.split_once(": ").unwrap();
+            let target = target.parse::<usize>().unwrap();
+            let others: Vec<usize> = remainder
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect();
+            match compute_all_combinations(others[0], &others[1..]).contains(&target) {
+                true => target,
+                false => 0,
+            }
+        })
+        .sum()
+}
+
+pub fn compute_all_combinations(carry_over: usize, others: &[usize]) -> Vec<usize> {
+    let mut sol = Vec::new();
+
+    let sol_1 = carry_over + others[0];
+    let sol_2 = carry_over * others[0];
+    let sol_3 = (carry_over.to_string() + &others[0].to_string())
+        .parse::<usize>()
+        .unwrap();
+
+    if others.len() > 1 {
+        sol.append(&mut compute_all_combinations(sol_1, &others[1..]));
+        sol.append(&mut compute_all_combinations(sol_2, &others[1..]));
+        sol.append(&mut compute_all_combinations(sol_3, &others[1..]));
+    } else {
+        sol.push(sol_1);
+        sol.push(sol_2);
+        sol.push(sol_3);
+    }
+
+    sol
 }
