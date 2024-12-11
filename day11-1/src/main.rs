eac77391cfb485c6dd09b43d6e620d0682a83179
Marvin Beckmann <mabeck@dtu.dk>
2024-12-11 13:50:10 +0100

    add solutions for day 11

--- day11-1/src/main.rs
+++ day11-1/src/main.rs
@@ -1,3 +1,47 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    println!("{}", solve_1(puzzle_input));
+}
+
+pub fn solve_1(puzzle_input: String) -> usize {
+    let mut input = puzzle_input;
+
+    for i in 0..25 {
+        input = input
+            .split_whitespace()
+            .fold("".to_string(), |acc, next_stone| {
+                acc + " " + &apply_transformation_rules(next_stone)
+            });
+        println!("{input}");
+    }
+
+    input.split_whitespace().count()
+}
+
+fn apply_transformation_rules(string: &str) -> String {
+    if string == "0" {
+        "1".to_string()
+    } else if string.len() % 2 == 0 {
+        string[0..(string.len()) / 2].to_owned()
+            + " "
+            + string[(string.len()) / 2..(string.len() - 1)].trim_start_matches("0")
+            + &string[(string.len() - 1)..]
+    } else {
+        let int_value = string.parse::<usize>().unwrap();
+        let new_value = int_value * 2024;
+        new_value.to_string()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::solve_1;
+
+    #[test]
+    fn test() {
+        let input = "125 17";
+        solve_1(input.to_owned());
+    }
 }
--- day11-2/src/main.rs
+++ day11-2/src/main.rs
@@ -1,3 +1,50 @@
+use std::{collections::HashMap, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    println!("{}", solve_2(puzzle_input));
+}
+
+pub fn solve_2(puzzle_input: String) -> usize {
+    let mut hash = HashMap::new();
+    puzzle_input
+        .split_whitespace()
+        .map(|x| apply_transformation_rules(x.to_owned(), 0, &mut hash))
+        .sum()
+}
+
+fn apply_transformation_rules(
+    string: String,
+    depth: usize,
+    already_computed: &mut HashMap<(String, usize), usize>,
+) -> usize {
+    if !already_computed.contains_key(&(string.clone(), depth)) {
+        let mut value = 0;
+
+        if depth == 75 {
+            value = 1;
+        } else if &string == "0" {
+            value = apply_transformation_rules("1".to_owned(), depth + 1, already_computed);
+        } else if &string.len() % 2 == 0 {
+            value = apply_transformation_rules(
+                string.clone()[0..(string.len()) / 2].to_string(),
+                depth + 1,
+                already_computed,
+            ) + apply_transformation_rules(
+                string[(string.len()) / 2..(string.len() - 1)]
+                    .trim_start_matches("0")
+                    .to_owned()
+                    + &string.clone()[(string.len() - 1)..],
+                depth + 1,
+                already_computed,
+            );
+        } else {
+            let int_value = string.parse::<usize>().unwrap();
+            let new_value = int_value * 2024;
+
+            value = apply_transformation_rules(new_value.to_string(), depth + 1, already_computed);
+        }
+        already_computed.insert((string.clone(), depth), value);
+    }
+    already_computed.get(&(string, depth)).unwrap().clone()
 }
